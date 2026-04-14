91892e41793ecc7e946723dcb7e0553d628b1a54
barbun <robarcover@gmail.com>
2026-04-14 12:16:09 +1000

    Added page_cache_kill_switch to banned calls.

--- tests/bats/validate/fixtures/banned_functions/Drupal.php
+++ tests/bats/validate/fixtures/banned_functions/Drupal.php
@@ -10,4 +10,16 @@ class Drupal {
    */
   public static function httpClient() {
   }
+
+  /**
+   * Retrieves a service from the container.
+   *
+   * @param string $id
+   *   The service identifier.
+   *
+   * @return object
+   *   The service instance.
+   */
+  public static function service($id) {
+  }
 }
